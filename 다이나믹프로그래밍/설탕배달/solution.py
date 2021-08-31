eb76d4e1c7a869bdf64e24cdcba788b12841f124
luboolu <kjjy98@naver.com>
2021-08-31 21:59:23 +0900

    210831

--- 다이나믹프로그래밍/설탕배달/solution.py
+++ 다이나믹프로그래밍/설탕배달/solution.py
@@ -1,20 +1,18 @@
 #BAEKJOON 2839번
-
 n = int(input())
 result = 0
 
 ##n = 3a + 5b 인 경우를 찾아야함
 if n % 5 == 0:
     result = n // 5
-elif n % 3 == 0:
-    result = n // 3
+
 else:
     while True:
-        if n % 3 != 0:
-            n -= 5
+        if n % 5 != 0:
+            n -= 3
             result += 1
         else:
-            n -= 3
+            n -= 5
             result += 1
 
         if n == 0:
