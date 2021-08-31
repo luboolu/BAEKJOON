ec4217a6c9161fdec888655dc7381876580fe8e4
luboolu <kjjy98@naver.com>
2021-08-31 18:54:48 +0900

    210831

--- 다이나믹프로그래밍/설탕배달/solution.py
+++ 다이나믹프로그래밍/설탕배달/solution.py
@@ -5,14 +5,14 @@ result = 0
 
 ##n = 3a + 5b 인 경우를 찾아야함
 
-while n >= 0:
-    result += 1
-    if n - 5 >= 0:
+while True:
+    if n % 5 == 0:
         n -= 5
-    elif n - 3 >= 0:
+        result += 1
+    elif n % 3 == 0:
         n -= 3
+        result += 1
     else:
-        result = -1
-        break
+        pass
 
 print(result)
