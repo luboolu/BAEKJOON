f90332f5c6f07cc19396f0f596f28faf47cd79ef
luboolu <kjjy98@naver.com>
2021-09-01 00:02:21 +0900

    210831

--- 다이나믹프로그래밍/1로만들기/solution.py
+++ 다이나믹프로그래밍/1로만들기/solution.py
@@ -2,29 +2,20 @@
 x = int(input())
 answer = 0
 
-a1, a2 = divmod(x, 3)
-b1, b2 = divmod(x, 2)
-
-if a1 < b1:
-    s = [3,2]
-else:
-    s = [2,3]
-
 while True:
-    if x % s[0] == 0:
-        x = x // s[0]
-    elif x % s[1] == 0:
-        x = x // s[1]
+    if x == 1:
+        break
+
+    if x % 2 == 0 and (x // 2) % 2 == 0:
+        x = x // 2
+    elif x % 3 == 1:
+        x -= 1
+    elif x % 3 == 0:
+        x = x // 3
+    elif x % 2 == 0:
+        x = x // 2
     else:
         x -= 1
     answer += 1
     print(x)
-
-    if x == 1:
-        break
-
 print(answer)
-
-
-
-
